39d7bd579a660648c7a18a95129c94aabfa46bcc
Vagiza Svidchenkova <w.svidchenkova@gmail.com>
2025-07-29 22:46:38 +0400

    Подключает App в intex

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './store';
+import HistoryRouter from './components/history-router/history-router';
+import browserHistory from './browser-history';
+import { ToastContainer } from 'react-toastify';
+import App from './components/app/app';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -7,6 +13,11 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <h1>Hello, World!</h1>
+    <Provider store={store}>
+      <HistoryRouter history={browserHistory}>
+        <ToastContainer />
+        <App />
+      </HistoryRouter>
+    </Provider>
   </React.StrictMode>
 );
